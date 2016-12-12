6c07fa5deae40cd8020619540390669cd259bb0c
Ridzuan Mohamad <ridzuanmo@gmail.com>
2016-12-12 09:11:59 +0800

    plotting total emissions PM2.5 by year

--- plot1.R
+++ plot1.R
@@ -1,5 +1,9 @@
-
+# load NEI datasource
 NEI <- readRDS("summarySCC_PM25.rds")
 
+# create vector with total PM2.5 by year
 e2y <- with(NEI, tapply(Emissions, year, sum, na.rm=TRUE))
-barplot(e2y, main="Total Emission of PM2.5", xlab="Year", ylab="PM2.5")
+
+png("plot1.png",width=480,height=480,units="px")
+    barplot(e2y, main="Total Emission of PM2.5", xlab="Year", ylab="PM2.5")
+dev.off()
